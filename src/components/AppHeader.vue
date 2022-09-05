18cf402505cb295b0f0ad7400c3dc8bdbfbaa15d
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-09-05 11:45:14 +0200

    start: Inizializzazione del progetto

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>header</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <h1>main</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
