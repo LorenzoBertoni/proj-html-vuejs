7deb6f110300a217568a4c9712f279e2cf9ef730
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-09-05 13:22:20 +0200

    add: elementi placeholder dell'header. style: creato il layout dell'header + file di regole generali da riutilizzare.

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,6 @@ export default {
 </script>
 
 <style lang="scss">
+@import './styles/general.scss';
 
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,47 @@
 <template>
     <header>
-        <h1>header</h1>
+        <div class="top-header">
+            <div class="d-flex-between boxed">
+                <p>lorem ipsum</p>
+                <p>lorem ipsum sit dolor amet</p>
+            </div>
+        </div>
+
+        <div class="bottom-header">
+            <div class="d-flex-between boxed">
+                <div class="logo-wrapper">
+                    <a href="#">
+                        <img 
+                        src="../assets/img/avada-drivers-logo-1x.png"
+                        alt="logo for Avada driving school"
+                        >
+                    </a>
+                </div>
+                
+                <nav class="nav-bar">
+                    <ul class="d-flex-end">
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>
+                            <a href="#" class="btn">btn</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <section id="jumbotron">
+            <div class="jumbo-wrapper boxed d-flex-start">
+                <div class="jumbo-text">
+                    <h1>lorem Ipsum sit dolor</h1>
+                    <p>lorem ipsum</p>
+                </div>
+            </div>
+        </section>
     </header>
 </template>
 
@@ -11,5 +52,66 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../styles/vars.scss';
 
+    .top-header {
+        background-color: $header_color;
+        padding: 1rem 0;
+
+        p {
+            color: $text_color;
+        }
+    }
+
+    .bottom-header {
+        background-color: gray;
+        opacity: 90%;
+        padding: 2.5rem;
+        position: fixed;
+        width: 100%;
+        top: 50px;
+        left: 0;
+        .logo-wrapper {
+            a {
+                img {
+                    width: 100%;
+                }
+            }
+        }
+        .nav-bar {
+            ul {
+                list-style: none;
+                gap: 4rem;
+                li {
+                    color: #fff;
+                }
+
+                .btn {
+                    background-color: $brand_color;
+                    color: $btn_text_color;
+                }
+            }
+        }
+    }
+
+    #jumbotron {
+        background-color: lightblue;
+        background-image: url('../assets/img/homepage-hero-background.jpg');
+        background-size: cover;
+        background-position: center;
+        padding: 20rem;
+        .jumbo-wrapper {
+            .jumbo-text {
+                color: $text_color;
+                h1 {
+                    font-size: 3rem;
+                    margin-bottom: .5rem;
+                }
+
+                p {
+                    font-size: 1.1rem;
+                }
+            }
+        }
+    }
 </style>
